cd9b8283a3380c328b0e25ed848e4ea0d8864fec
nguyen.trong.toan <nguyen.trong.toan@framgias-MacBook-Pro.local>
2018-10-29 10:43:26 +0700

    feat: update link

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const morgan = require('morgan')
 const api = require('./api')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const PORT = process.env.PORT || 3000
+const root = path.join(__dirname, 'build/')
+app.use(express.static(root))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,3 +19,6 @@ app.use(morgan('tiny'))
 app.listen(PORT, () => log(`Example app listening on port ${PORT}!`))
 
 app.use('/api', api)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'))
+})
